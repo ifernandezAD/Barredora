7ee3f4fa5f15b2154d5d26d895e04e866b2b007a
ifernandezAD <ifernandezad@gmail.com>
2021-09-01 17:26:20 +0200

    Modo trabajo simple

--- Assets/Scripts/MovimientoBarredora.cs
+++ Assets/Scripts/MovimientoBarredora.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 public class MovimientoBarredora : MonoBehaviour
 {
     Rigidbody myRigid;
-    
+
     //Variables de movimiento
     public float vel;
     public float rotationVel;
-    
+
+    public bool workModeOn = false;
+
     void Start()
     {
         myRigid = GetComponent<Rigidbody>();
     }
 
-   
+
     void Update()
     {
-        Movimiento();
+        Controls();
+
+        if (workModeOn)
+        {
+            WorkMode();
+        }
+        else
+        {
+            NormalMode();
+        }
+        
     }
 
-    void Movimiento()
+    void Controls()
     {
+        // The Machine movement
+
         if (Input.GetKey(KeyCode.W))
         {
-            myRigid.AddRelativeForce(Vector3.forward * vel * Time.deltaTime,ForceMode.VelocityChange);
+            myRigid.AddRelativeForce(Vector3.forward * vel * Time.deltaTime, ForceMode.VelocityChange);
 
         }
         if (Input.GetKey(KeyCode.S))
         {
-            myRigid.AddRelativeForce(-Vector3.forward * vel * Time.deltaTime,ForceMode.VelocityChange);
+            myRigid.AddRelativeForce(-Vector3.forward * vel * Time.deltaTime, ForceMode.VelocityChange);
 
         }
         if (Input.GetKey(KeyCode.A))
@@ -43,5 +57,23 @@ public class MovimientoBarredora : MonoBehaviour
             transform.Rotate(Vector3.up * rotationVel * Time.deltaTime);
 
         }
+
+        //Enable, disable WorkMode
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            workModeOn = !workModeOn;
+        }
+    }
+
+    void WorkMode()
+    {
+        Debug.Log("Work Mode On");
+    }
+
+    void NormalMode()
+    {
+        Debug.Log("Work Mode Off");
+
     }
 }
